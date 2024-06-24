14bea3fdc6edd1d716fc675c77599a81bb6c9d1c
marbh56 <nathan.a.sanders2@gmail.com>
2024-06-24 12:50:20 -0400

    Working on c1p1

--- main.go
+++ main.go
@@ -5,13 +5,40 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+var count int = 0
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	}
+}
+	
+	
+func getFilerserverCount(w http.RepsoneWritter, _ *http.Request){
+	
+}
+
+func getHealth(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	fmt.Print("starting server...")
 
-	// logoPath := "/assets/logo.png"
-	fs := http.FileServer(http.Dir("assets"))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	http.ListenAndServe(":8080", mux)
+	apiCfg := &apiConfig{}
 
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/app/*", api.Cfg.middlewareMetrics(http.StripPrefix("/app/", fs)))
+
+	mux.HandleFunc("/healthz", getHealth)
+
+	http.ListenAndServe(":8080", mux)
 }
