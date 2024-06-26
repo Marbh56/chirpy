954ac429300ecf9bf3fc268e7251343ba2b0279f
marbh56 <nathan.a.sanders2@gmail.com>
2024-06-26 19:22:35 -0400

    working on cleanUpChirp func

--- main.go
+++ main.go
@@ -1,26 +1,94 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type apiConfig struct {
 	fileserverHits int
 }
 
-var count int = 0
-
 func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) adminMetrics(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	visitCount := cfg.fileserverHits
+	htmlResponse := fmt.Sprintf(`
+	<html>
+
+	<body>
+    	<h1>Welcome, Chirpy Admin</h1>
+    	<p>Chirpy has been visited %d times!</p>
+	</body>
+
+	</html>
+	`, visitCount)
+
+	w.Write([]byte(htmlResponse))
+
+}
+
+func (cfg *apiConfig) validateChrip(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Something went wrong"})
+		return
+	}
+	if len(params.Body) <= 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		json.NewEncoder(w).Encode(map[string]bool{"valid": true})
+
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Chirp is too long"})
+	}
+
+}
+
+func (cfg *apiConfig) cleanUpChirp(w http.ResponseWriter, r *http.Request) {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	body := validateChrip(w, r)
+	cleanBody := strings.ToLower(body)
+	words := strings.Fields(cleanBody)
+	for i, word := range words {
+		for _, badWord := range badWords {
+			if word == badWord {
+				words[i] = "****"
+			}
+		}
 	}
 }
-	
-	
-func getFilerserverCount(w http.RepsoneWritter, _ *http.Request){
-	
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset successfully."))
 }
 
 func getHealth(w http.ResponseWriter, _ *http.Request) {
@@ -35,10 +103,19 @@ func main() {
 
 	apiCfg := &apiConfig{}
 
+	// File server for /app/ path
 	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/*", api.Cfg.middlewareMetrics(http.StripPrefix("/app/", fs)))
+	mux.Handle("/app/*", apiCfg.middlewareMetrics(http.StripPrefix("/app/", fs)))
 
-	mux.HandleFunc("/healthz", getHealth)
+	// Use the correct path setup with HTTP method in mux.HandleFunc
+	mux.HandleFunc("GET /api/healthz", getHealth)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.adminMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.validateChrip)
 
+	// Other handlers without method prefix
+	mux.HandleFunc("/api/reset", apiCfg.handleReset)
+
+	// Start the server
 	http.ListenAndServe(":8080", mux)
 }
