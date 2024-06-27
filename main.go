9ff92f351fe09b09b33bd6526292a8d477085dc8
marbh56 <nathan.a.sanders2@gmail.com>
2024-06-27 12:57:22 -0400

    need to work on getChirpHandler and adding it all into main

--- main.go
+++ main.go
@@ -5,12 +5,169 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"sync"
 )
 
 type apiConfig struct {
 	fileserverHits int
 }
+
+type Chirp struct {
+	ID   int    `json:"id"`
+	Body string `json:"body"`
+}
+
+type DBStructure struct {
+	Chirps map[int]Chirp `json:"chirps"`
+}
+
+
+type DB struct {
+	path string
+	mux  *sync.RWMutex
+	currentID int
+}
+
+func NewDB(path string) (*DB, error){
+	db := &DB{
+		path: path,
+		mux: &sync.RWMutex{},
+	}
+
+	err := db.ensureDB()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+	if len(data.Chrips) > 0 {
+		for id := range data.Chirps {
+			if id >= db.currentID {
+				db.currentID = id + 1
+			}
+		}
+	}
+
+	return db, nil
+
+}
+
+func (db *DB) ensureDB() error {
+	_, err := os.Stat(db.path)
+	if os.IsNotExist(err) {
+		dbStructure := DBStructure{Chrips: make(map[int]Chirp)}
+		return db.writeDB(&dbStructure)
+	}
+	return err
+}
+
+func (db *DB) loadDB() (DBStructure, error) {
+	db.mux.RLock()
+	defer db.mux.RUnlock()
+	bytes, err := os.ReadFile(db.path)
+	if err != nil {
+		return DBStructure{}, err
+	}
+
+	var dbStructure DBStructure
+	err = json.Unmarshal(bytes, &dbStructure)
+	if err != nil {
+		return DBStructure{}, err
+	}
+
+	return dbStructure, nil
+}
+
+func (db *DB) writeDB(dbStructure *DBStructure) error {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	bytes, err := json.MarshalIndent(dbStructure, "", " ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(db.path, bytes, 0644)
+}
+
+func (db *DB) CreateChirp(body string) (Chrip, error) {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	data, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	chirp := Chirp{
+		ID: db.currentID
+		Body: body,
+	}
+
+	data.Chirps[db.currentID] = chirp
+	db.currentID++
+
+	err = db.writeDB(&data)
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	return chirp, nil
+}
+
+func (db *DB) GetChirps() ([]Chirp, error) {
+	db.mux.RLock()
+	defer db.mux.RUnlock()
+
+	data, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+
+	chirps := make([]Chirp, 0, len(data.Chirps))
+	for _, chirp := range data.Chirps {
+		chirps = append(chirps, chirp)
+	}
+
+	sort.Slice(chirps, func(i, j int) bool {
+		return chirps[i].ID < chirps[j].ID
+	})
+
+	return chirps, nil
+}
+
+func createChirpHandler(db *DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var requestBody map[string]string
+		if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		 body, exist := requestBody["body"]
+		 if !exist || len(body) == 0 {
+			http.Error(w, "Invalid chirp body", http.StatusBadRequest)
+			return
+		 }
+
+		 chirp, err := db.CreateChirp(body)
+		 if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		 }
+
+		 w.WriteHeader(http.StatusCreated)
+		 if err := json.NewEncoder(w).Encode(chirp); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		 }
+		 
+	}
+}
+
 
 func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -37,7 +194,7 @@ func (cfg *apiConfig) adminMetrics(w http.ResponseWriter, _ *http.Request) {
 
 }
 
-func (cfg *apiConfig) validateChrip(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) isValidChrip(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
@@ -84,6 +241,8 @@ func (cfg *apiConfig) validateChrip(w http.ResponseWriter, r *http.Request) {
 
 }
 
+
+
 func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
@@ -116,7 +275,10 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", getHealth)
 	mux.HandleFunc("GET /api/metrics", apiCfg.handleMetrics)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.adminMetrics)
-	mux.HandleFunc("POST /api/validate_chirp", apiCfg.validateChrip)
+
+	// Handlers for the act of chirping
+	mux.HandleFunc("POST /api/chirps", ????)
+	mux.HandleFunc("GET /api/chirps", ????)
 
 	// Other handlers without method prefix
 	mux.HandleFunc("/api/reset", apiCfg.handleReset)
